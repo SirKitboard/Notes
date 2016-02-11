c9032edbc4a3235d6af932c4ecd535ec95b9f62f
Aditya Balwani <adibalwani@gmail.com>
2016-02-11 10:45:11 -0500

    Part one of CSE337 homework

---
+++ CSE337/Assignment1/q1.py
@@ -0,0 +1,19 @@
+file_name = input("Input file name :")
+content = []
+
+f = open(file_name)
+for line in f:
+    line = line.replace('<span>', '')
+    line = line.replace('</span>', '')
+    # print(line)
+    content.append(line)
+
+splitFileName = file_name.split('.')
+if(len(splitFileName) > 1):
+    newFileName = splitFileName[0] + '_output.' + splitFileName[1]
+else:
+    newFileName = splitFileName[0] + '_output'
+
+f = open(newFileName, "w")
+for line in content:
+    f.write(line)
